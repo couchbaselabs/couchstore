couchbaselabs/couchstore

commit 9b3dad6f47cfe2bf5055512c720f5d535ed7139f

MB-5710 add COUCHSTORE_ERROR_CANCEL
CouchKVStore uses this new error code to stop loading kv pairs from
couchstore.

Change-Id: I65b16c0229192435b2678af767edf746430a4975
Reviewed-on: http://review.couchbase.org/17681
Reviewed-by: Jens Alfke <jens@couchbase.com>
Tested-by: Jin Lim <jin@couchbase.com>
Reviewed-by: Aaron Miller <apage43@ninjawhale.com>

src/couch_db.c
@@ -532,12 +532,12 @@ static couchstore_error_t lookup_callback(couchfile_lookup_request *rq,
     } else {
         by_seq_read_docinfo(&docinfo, seqterm, v);
     }
-    
-    if (context->callback(context->db, docinfo, context->callback_context) == 0) {
+
+    int rc = context->callback(context->db, docinfo, context->callback_context);
+    if (rc <= 0) {
         couchstore_free_docinfo(docinfo);
     }
-
-    return COUCHSTORE_SUCCESS;
+    return rc;
 }
 
 LIBCOUCHSTORE_API

include/libcouchstore/couch_db.h
@@ -291,14 +291,15 @@ extern "C" {
      * and couchstore_docinfos_by_sequence() to iterate through the documents.
      *
      * The docinfo structure is automatically freed if the callback
-     * returns 0. A non-zero return value will preserve the DocInfo
+     * returns 0. A positive return value will preserve the DocInfo
      * for future use (should be freed with free_docinfo by the
-     * caller)
+     * caller). A negative return value will cancel the iteration and
+     * pass the error value back to the caller.
      *
      * @param db the database being traversed
      * @param docinfo the current document
      * @param ctx user context
-     * @return 1 to preserve the DocInfo, 0 to free it (see above).
+     * @return 1 to preserve the DocInfo, 0 or negative error value to free it (see above).
      */
     typedef int (*couchstore_changes_callback_fn)(Db *db,
                                                   DocInfo *docinfo,

include/libcouchstore/error.h
@@ -23,7 +23,8 @@ extern "C" {
         COUCHSTORE_ERROR_HEADER_VERSION = -8,
         COUCHSTORE_ERROR_CHECKSUM_FAIL = -9,
         COUCHSTORE_ERROR_INVALID_ARGUMENTS = -10,
-        COUCHSTORE_ERROR_NO_SUCH_FILE = -11
+        COUCHSTORE_ERROR_NO_SUCH_FILE = -11,
+        COUCHSTORE_ERROR_CANCEL = -12
     } couchstore_error_t;
 
 #ifdef __cplusplus

commit d1fe368c93112267993416e6b8a20d2c0ff920e3

Return COUCHSTORE_SUCCESS when not freeing docinfo
This fixes a regression from commit
9b3dad6f47cfe2bf5055512c720f5d535ed7139f

Change-Id: Ib8b92e1188549bc9ac96f943e3b6f0f7d238548f
Reviewed-on: http://review.couchbase.org/17725
Reviewed-by: Jin Lim <jin@couchbase.com>
Tested-by: Aaron Miller <apage43@ninjawhale.com>

src/couch_db.c
@@ -536,6 +536,9 @@ static couchstore_error_t lookup_callback(couchfile_lookup_request *rq,
     int rc = context->callback(context->db, docinfo, context->callback_context);
     if (rc <= 0) {
         couchstore_free_docinfo(docinfo);
+    } else {
+        // User requested docinfo not be freed, don't free it, return success
+        return COUCHSTORE_SUCCESS;
     }
     return rc;
 }