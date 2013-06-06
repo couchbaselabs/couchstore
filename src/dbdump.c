1821d85ec0caeb8c585cbfdd67194960e98133e2
Aaron Miller <apage43@ninjawhale.com>
2013-06-06 11:21:45 -0700

    MB-100 Fix dbdump flag handling
    Change-Id: I6660eff6d0c5c5b4acd5b92778bf73ee4ee282d2
    Reviewed-on: http://review.couchbase.org/26792
    Reviewed-by: Fulu Li <fulu@couchbase.com>
    Tested-by: Aaron Miller <apage43@ninjawhale.com>

--- src/dbdump.c
+++ src/dbdump.c
@@ -207,8 +207,6 @@ int main(int argc, char **argv)
         usage();
     }
 
-    DumpMode mode = DumpBySequence;
-
     int ii = 1;
     while (strncmp(argv[ii], "-", 1) == 0) {
         if (strcmp(argv[ii], "--byid") == 0) {
