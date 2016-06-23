be4ba53edff0c01750fe128ec2612abb651cbe44
Dave Rigby <daver@couchbase.com>
2016-06-23 14:00:31 +0100

    Example python script to create couchstore file
    Change-Id: Iadca33234769b88b00f76c075858b31a1b289658
    Reviewed-on: http://review.couchbase.org/65193
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Trond Norbye <trond.norbye@gmail.com>

---
+++ python/example1.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python2.7
+
+# Simple example program to create a Couchstore file with a given
+# number of keys.
+
+from couchstore import CouchStore, DocumentInfo, SizedBuf
+import os
+import struct
+import sys
+
+BATCH_SIZE = 10000
+
+
+def insert(db, key, rev, value):
+    REV_META_PACK = ">QII"
+    info = DocumentInfo(key)
+    info.revSequence = rev
+    # cas, exp, flags
+    info.revMeta = str(struct.pack(REV_META_PACK, 1, 2, 3))
+    info.deleted = False
+    return db.save(info, value)
+
+
+def insert_multi(db, keys, values):
+    """Inserts multiple keys / values."""
+
+    ids = []
+    for k in keys:
+        info = DocumentInfo(k)
+        info.revSequence = 1
+        # cas, exp, flags
+        info.revMeta = str(struct.pack(REV_META_PACK, 1, 2, 3))
+        info.deleted = False
+        ids.append(info)
+    return db.saveMultiple(ids, values)
+
+
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in range(0, len(l), n):
+        yield l[i:i+n]
+
+
+def main():
+    db = CouchStore(sys.argv[2], 'c')
+    for batch in chunks(range(0, int(sys.argv[1])), BATCH_SIZE):
+        insert_multi(db,
+                     ["key_" + str(x) for x in batch],
+                     [str(x) for x in batch])
+        db.commit()
+    db.close()
+
+if __name__ == "__main__":
+    main()
